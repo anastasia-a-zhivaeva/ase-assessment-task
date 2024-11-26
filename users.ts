de263f317c0ab2e842e9bc0b09ce954eac585e2f
Anastasiia Zhivaeva <anastasia.a.zhivaeva@gmail.com>
2024-11-26 17:55:01 -0500

    initial commit

---
+++ users.ts
@@ -0,0 +1,69 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, GetCommand, PutCommand } from "@aws-sdk/lib-dynamodb";
+import express from "express";
+import ServerlessHttp from "serverless-http";
+
+const USERS_TABLE = process.env.USERS_TABLE;
+const client = new DynamoDBClient();
+const docClient = DynamoDBDocumentClient.from(client);
+
+const app = express();
+app.use(express.json());
+
+app.get("/users/:userId", async (req, res) => {
+  const params = {
+    TableName: USERS_TABLE,
+    Key: {
+      userId: req.params.userId,
+    },
+  };
+
+  try {
+    const command = new GetCommand(params);
+    const { Item } = await docClient.send(command);
+    if (Item) {
+      const { userId, name } = Item;
+      res.json({ userId, name });
+    } else {
+      res
+        .status(404)
+        .json({ error: "Could not find user with provided 'userId'" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Could not retrieve user" });
+  }
+});
+
+app.post("/users", async (req, res) => {
+  const { userId, name } = req.body;
+  if (typeof userId !== "string") {
+    res.status(400).json({ error: "'userId' must be a string" });
+  } else if (typeof name !== "string") {
+    res.status(400).json({ error: "'name' must be a string" });
+  }
+
+  const params = {
+    TableName: USERS_TABLE,
+    Item: { userId, name },
+  };
+
+  try {
+    const command = new PutCommand(params);
+    await docClient.send(command);
+    res.json({ userId, name });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Could not create user" });
+  }
+});
+
+app.use((req, res, next) => {
+  console.log(req);
+
+  res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+export const handler = ServerlessHttp(app);
